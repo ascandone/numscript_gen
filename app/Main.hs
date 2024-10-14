ed6800ab48498cabf042961002d6201d1d61915b
Alessandro Scandone <alescandone@gmail.com>
2024-10-14 11:25:00 +0200

    generate scripts

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Text.IO (putStrLn)
+import Numscript.Format (format)
+import Numscript.Gen (generateProgram)
+import Prelude hiding (putStrLn)
+
 main :: IO ()
-main = return ()
+main = do
+  putStrLn "generating.."
+  program <- generateProgram
+  let fmt = format program
+  putStrLn fmt
--- src/Numscript/Gen.hs
+++ src/Numscript/Gen.hs
@@ -1,21 +1,129 @@
-module Numscript.Gen where
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-import Control.Applicative (liftA3)
+{-# HLINT ignore "Use <$>" #-}
+
+module Numscript.Gen (program, generateProgram) where
+
+import Control.Monad (replicateM)
+import Data.Ratio ((%))
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Numscript
 import Test.QuickCheck
 
-data Tree
-  = Empty
-  | Node Tree Int Tree
-  deriving (Eq, Show)
+isNonNegative :: (Ord a, Num a) => a -> Bool
+isNonNegative x = x >= 0
 
-bt :: Gen Tree
-bt =
+isPositive :: (Ord a, Num a) => a -> Bool
+isPositive x = x > 0
+
+toText :: (Show a) => a -> Text
+toText = T.pack . show
+
+nonEmptyVectorOf :: Int -> Gen a -> Gen [a]
+nonEmptyVectorOf len g = do
+  s <- choose (1, len)
+  replicateM s g
+
+monetary :: Gen Numscript.Monetary
+monetary = do
+  Numscript.Monetary "COIN" <$> arbitrary `suchThat` isNonNegative
+
+portion :: Gen Rational
+portion = do
+  den <- arbitrary `suchThat` isPositive
+  num <- choose (0, den)
+  return $ num % den
+
+overdraft :: Gen (Maybe Numscript.Monetary)
+overdraft =
   oneof
-    [ return Empty
-    , liftA3 Node bt arbitrary bt
+    [ return Nothing
+    , Just <$> monetary
     ]
 
-main :: IO ()
-main = do
-  x <- generate bt
-  print x
+account :: Gen Text
+account = do
+  k <- choose (0 :: Int, 5)
+  return $ "acc" <> toText k
+
+source :: Int -> Gen Numscript.Source
+source s =
+  oneof $
+    if s <= 0
+      then base
+      else base ++ rec_
+ where
+  base =
+    [ return Numscript.SrcAccount
+        <*> account
+    , return Numscript.SrcAccountOverdraft
+        <*> account
+        <*> overdraft
+    , return Numscript.SrcCapped
+        <*> monetary
+        <*> source (s - 1)
+    ]
+  rec_ =
+    [ return Numscript.SrcInorder
+        <*> nonEmptyVectorOf s (source (s - 1))
+    , return Numscript.SrcAllotment
+        <*> nonEmptyVectorOf s (allotmentClause (source $ s - 1))
+    ]
+
+allotmentClause :: Gen a -> Gen (Numscript.AllotmentClause a)
+allotmentClause x =
+  return Numscript.AllotmentClause
+    <*> portion
+    <*> x
+
+destination :: Int -> Gen Numscript.Destination
+destination s =
+  oneof $
+    if s <= 0
+      then base
+      else base ++ rec_
+ where
+  base =
+    [ return $ Numscript.DestAccount "addr"
+    ]
+  rec_ =
+    [ return Numscript.DestInorder
+        <*> nonEmptyVectorOf s (destinationInorderClause (s - 1))
+        <*> keptOrDest (s - 1)
+    , return Numscript.DestAllotment
+        <*> nonEmptyVectorOf s (allotmentClause (keptOrDest $ s - 1))
+    ]
+
+destinationInorderClause :: Int -> Gen (Numscript.Monetary, Numscript.KeptOrDest)
+destinationInorderClause s =
+  return (,)
+    <*> monetary
+    <*> keptOrDest s
+
+keptOrDest :: Int -> Gen Numscript.KeptOrDest
+keptOrDest s =
+  oneof
+    [ return Numscript.Kept
+    , return Numscript.To
+        <*> destination s
+    ]
+
+statement :: Gen Numscript.Statement
+statement = do
+  amt <- monetary
+  src <- source 4
+  dest <- destination 4
+  return $
+    Numscript.Send
+      { Numscript.amount = amt
+      , Numscript.source = src
+      , Numscript.destination = dest
+      }
+
+program :: Gen Numscript.Program
+program = listOf statement
+
+generateProgram :: IO Numscript.Program
+generateProgram = generate program
